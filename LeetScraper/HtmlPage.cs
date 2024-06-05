4f6d87a2c831ec948d7b6b2d37cfff67651d12d1
Jimmy <vaugh@hotmail.se>
2024-06-05 06:45:29 +0200

    Find links

---
+++ LeetScraper/HtmlPage.cs
@@ -0,0 +1,21 @@
+﻿using HtmlAgilityPack;
+
+namespace LeetScraper;
+
+public class HtmlPage
+{
+    private readonly HtmlDocument _htmlDocument;
+    
+    public HtmlPage(string html)
+    {
+        _htmlDocument = new HtmlDocument();
+        _htmlDocument.LoadHtml(html);
+    }
+
+    public IEnumerable<string> ListLinks()
+    {
+       return _htmlDocument.DocumentNode
+            .SelectNodes("//a[@href]")
+            .Select(linkNode => linkNode.GetAttributeValue("href", ""));
+    }
+}
---
+++ Tests/FindLinks.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using LeetScraper;
+
+namespace Tests;
+
+[TestFixture]
+public class FindLinks
+{
+    [Test]
+    public void Ahref()
+    {
+        var htmlPage = new HtmlPage("<html><body><a href=\"page.html\">link</a>");
+        var links = htmlPage.ListLinks();
+        links.Should().HaveCount(1);
+    }
+}
